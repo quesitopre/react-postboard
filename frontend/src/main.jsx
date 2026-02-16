569b3284ce684fae8d0ce5d86b68a630a0b85d19
quesitopre <citlallimartinez122@gmail.com>
2026-02-16 12:42:05 -0800

    Added router and paths

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+
 import App from './App'
+import NewPost from './components/NewPost';
+import './index.css'
+
+const router = createBrowserRouter([ // an array that takes an object that has one route and compent that should be loaded  
+  {path:'/', element: <App/> }, //our domain
+  {path:'/create-post', element: <NewPost/>} // if the user tries to access a route that is not defined, they will see a message that says "Page not found"
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router ={router}/>
   </React.StrictMode>
 )
