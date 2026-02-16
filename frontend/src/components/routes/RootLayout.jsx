967ff3ad9b746cb71434203a665179a146e5480a
quesitopre <citlallimartinez122@gmail.com>
2026-02-16 13:05:34 -0800

    imported rootLayout so that the nested routes can render contents.

--- frontend/src/components/routes/RootLayout.jsx
+++ frontend/src/components/routes/RootLayout.jsx
@@ -1,9 +1,12 @@
+import {Outlet} from 'react-router-dom';
+
 import MainHeader from "../MainHeader";
 
 function RootLayout(){
     return (
     <>
     <MainHeader />
+    <Outlet /> {/* this is where the child routes will be rendered */}
     </>
     );
 }
