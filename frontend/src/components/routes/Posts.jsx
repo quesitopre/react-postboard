f86bae46d54ba72acd5e1a9f410a2e1e10a874bd
quesitopre <citlallimartinez122@gmail.com>
2026-02-17 18:23:13 -0800

    Renamed App to Posts. refactored Posts file

---
+++ frontend/src/components/routes/Posts.jsx
@@ -0,0 +1,14 @@
+//root component
+import PostsList from "../PostsList";
+
+function Posts() {
+  return (
+  <>
+    
+    <main>
+    <PostsList/>
+  </main>
+  </>
+  );
+}
+export default Posts;
