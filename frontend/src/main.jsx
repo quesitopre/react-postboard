7ee139141d43eff7cd3b01abbe7f7260c9d804fd
quesitopre <citlallimartinez122@gmail.com>
2026-02-18 10:54:13 -0800

    added PD jsx & css.& fetched data from backend via id

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
 import Posts, {loader as postsLoader} from './components/routes/Posts';
 import NewPost, {action as newPostAction} from './components/routes/NewPost';
+import PostDetails, {loader as postDetailsLoader } from './components/routes/PostDetails';
 import RootLayout from './components/routes/RootLayout';
 import './index.css'
 
@@ -16,9 +17,11 @@ const router = createBrowserRouter([ // an array that takes an object that has o
       element: <Posts/>, 
       loader: postsLoader, // will execute function when the user acesses element router 
       children:[
-        {path:'/create-post', element: <NewPost/>, action: newPostAction }], // if the user tries to access a route that is not defined, they will see a message that says "Page not found"
-    },
-   ],
+        {path:'/create-post', element: <NewPost/>, action: newPostAction }, // if the user tries to access a route that is not defined, they will see a message that says "Page not found"
+        {path:'/:id', element:<PostDetails/>, loader: postDetailsLoader }// dynamic route with a dynamic route param
+      ],
+      },
+    ],
    },
 ]);
 
